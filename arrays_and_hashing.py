ef5bf356b01e34888fc4ad2904eaf2bc2e23720e
MechamJonathan <jonsmec95@gmail.com>
2025-05-17 06:24:22 -0600

    two sum

--- arrays_and_hashing.py
+++ arrays_and_hashing.py
@@ -26,3 +26,14 @@ class ArraysAndHashing():
         if countS != countT:
             return False
         return True
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        previousNums = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in previousNums:
+                return [previousNums[diff], i]
+            else:
+                previousNums[n] = i
--- test_arrays_and_hashing.py
+++ test_arrays_and_hashing.py
@@ -17,12 +17,17 @@ class TestArraysAndHashing(unittest.TestCase):
     def test_contains_duplicate_false(self):
         self.assertFalse(self.solver.contains_duplicate([1, 2, 3, 4]))
 
-    #valid anagrams
+    # valid anagrams
     def test_valid_anagram_true(self):
         self.assertTrue(self.solver.isAnagram("racecar", "carrace"))
 
     def test_valid_anagram_false(self):
         self.assertFalse(self.solver.isAnagram("jar", "jam"))
+    
+    # two sum
+    def test_two_sum_basic(self):
+        result = self.solver.twoSum([2, 7, 11, 15], 9)
+        self.assertEqual(sorted(result), [0,1])
 
 if __name__ == '__main__':
     unittest.main()
