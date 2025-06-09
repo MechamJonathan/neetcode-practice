a51eee976e4ebc7da6dacae320967047dd5fc9c2
MechamJonathan <jonsmec95@gmail.com>
2025-06-09 08:02:46 -0600

    reset

--- stacks.py
+++ stacks.py
@@ -2,16 +2,5 @@
 
 class Stacks():
     def isValidParenthesis(self, s:str) -> bool:
-        stack = []
-        closeToOpen = {")":"(", "]":"[", "}":"{"}
-
-        for c in s:
-            if c in closeToOpen:
-                if len(stack) != 0 and stack[-1] == closeToOpen[c]:
-                    stack.pop()
-                else:
-                    return False
-            else:
-                stack.append(c)
+        return
         
-        return True if len(stack) == 0 else False 
