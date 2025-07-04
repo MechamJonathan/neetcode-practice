e1abfb654ae3dcd31b44715f88dc8b1beafdce14
MechamJonathan <jonsmec95@gmail.com>
2025-07-04 04:36:35 -0600

    reset

--- two_pointer.py
+++ two_pointer.py
@@ -4,17 +4,7 @@ from typing import List
 
 class TwoPointer():
     def isPalindrome(self, s: str) -> bool:
-        cleaned = "".join(char for char in s if char.isalnum()).lower()
-
-        l, r = 0, len(cleaned) - 1
-
-        while l < r:
-            if cleaned[l] != cleaned[r]:
-                return False
-            else:
-                l += 1
-                r -= 1
-        return True
+        return
     
     def twoSumII(self, numbers: List[int], target: int) -> List[int]:
         l, r = 0, len(numbers) - 1
