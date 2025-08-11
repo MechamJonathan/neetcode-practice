deea69c969fe880689eac27b210f1f26859d0ff2
MechamJonathan <jonsmec95@gmail.com>
2025-08-11 13:28:10 -0600

    search matrix

--- binary_search.py
+++ binary_search.py
@@ -16,4 +16,25 @@ class BinarySearch():
             else:
                 high = median - 1
         return -1
+    
+    #Search Matrix
+    def search_matrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if self.binary_search(row, target):
+                return True
+        return False
+    
+    def binary_search(self, row, target):
+        low, high = 0, len(row) - 1
+
+        while low <= high:
+            median = (low + high) // 2
+
+            if row[median] == target:
+                return True
+            elif row[median] < target:
+                low = median + 1
+            else:
+                high = median - 1
+        return False
         
--- test_binary_search.py
+++ test_binary_search.py
@@ -14,5 +14,11 @@ class TestBinarySearch(unittest.TestCase):
         target = 4
         self.assertTrue(self.solver.search(nums, target), 3)
 
+    def test_search_matrix(self):
+        matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+        target = 3
+        self.assertTrue(self.solver.search_matrix(matrix, target), 3)
+
+
 if __name__ == '__main__':
     unittest.main()
